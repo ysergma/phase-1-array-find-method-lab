6281b7839e0dfed31d8772f711026637bbf2bb8a
Youssouf Sergma <sil3nthilldz@gmail.com>
2023-08-14 22:42:48 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+const record = [
+   {year: "2018", result: "L"},
+   {year: "2017", result: "W"},
+   {year: "2016", result: "N/A"}
+
+ ]
+ 
+ function isCherries(fruit) {
+   return fruit.result === "W";
+ }
+ 
+ console.log(record.find(isCherries));
+ const found = function(){ return record.find(isCherries)}
+  const superbowlWin = function(record){
+   if(record.find(isCherries)){
+      return record.find(isCherries)
+                  .year
+   }else {
+      return undefined
+   }
+   
+        
+}
